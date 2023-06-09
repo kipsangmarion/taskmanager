ed0245399b95443d71366dd401df541bf273056b
kipsangmarion <kipsangmarion@gmail.com>
2023-06-09 18:51:56 +0300

    registered models

--- taskmaster/admin.py
+++ taskmaster/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserProfile
+from .models import Task
+from .models import Tag
+from .models import Activity
+from .models import Comment
+
+admin.site.register(UserProfile)
+admin.site.register(Task)
+admin.site.register(Tag)
+admin.site.register(Activity)
+admin.site.register(Comment)
