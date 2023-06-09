c56fe16af73f2989c5b9a9f960e3a85cf573c030
kipsangmarion <kipsangmarion@gmail.com>
2023-06-09 19:41:27 +0300

    corrected error on app registration

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'taskmanager.apps.TaskmasterConfig'
+    'taskmaster.apps.TaskmasterConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
